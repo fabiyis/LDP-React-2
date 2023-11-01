b9f0cf303db26972af892825e36d71ba22a2fcc2
fabiyis <141014211+fabiyis@users.noreply.github.com>
2023-11-01 02:28:09 -0700

    more styling in NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ function NavBar() {
       <>
         <nav className="bg-black fixed h-16 w-full flex justify-between items-center px-3 fontFamily:Segoe, Tahoma, sans-serif" >
             <h1 className="text-3xl font-bold text-amber-500">fabXchange$</h1>
-            <div className="flex items-center gap-10">
+            <div className="flex items-center gap-10 space-y-1 px-2 pb-3 pt-2">
                 <a 
                 href="." className="text-white text-sm font-semibold hover:text-amber-500">
                     Home
