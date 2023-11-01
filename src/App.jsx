b6e4942ba9a87fadd3e0bfb65ef7db697f561f1d
fabiyis <141014211+fabiyis@users.noreply.github.com>
2023-11-01 01:32:27 -0700

    add a contents to Body components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
+import Body from "./components/Body";
 
 function App() {
   return (
     <>
      <NavBar />
      <SideBar />
+     <Body />
     </>
   )
 }
