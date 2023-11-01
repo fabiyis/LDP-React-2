d5e17e58d24c327146d52674e06459a8c39f34f9
fabiyis <141014211+fabiyis@users.noreply.github.com>
2023-11-01 00:44:53 -0700

    add Home,About,Market and Contact link in NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+function NavBar() {
+    return (
+      <>
+        <nav className="bg-black fixed h-16 w-full flex justify-between items-center px-3 fontFamily:Segoe, Tahoma, sans-serif" >
+            <h1 className="text-3xl font-bold text-amber-500">fabXchange$</h1>
+            <div className="flex items-center gap-10">
+                <a 
+                href="." className="text-white text-sm font-semibold hover:text-amber-500">
+                    Home
+                </a>
+                <a 
+                href="." className="text-white text-sm font-semibold hover:text-amber-500">
+                    About
+                </a>
+                <a 
+                href="." className="text-white text-sm font-semibold hover:text-amber-500">
+                    Market
+                </a>
+                <a 
+                href="." className="text-white text-sm font-semibold hover:text-amber-500">
+                    Contact
+                </a>
+
+            </div>
+        </nav>
+      </>
+    )
+  }
+  
+  export default NavBar;
